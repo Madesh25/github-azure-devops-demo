b55d42e47265c1d0ea7c48cca1c84b18c2348d1e
Madesh <119011846+Madesh25@users.noreply.github.com>
2026-08-20 17:04:27 +0530

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,8 @@
+==============================
+   GitHub DevOps Demo App
+==============================
+
+Application Name : GitHub Demo
+Environment      : Development
+
+Application started successfully.
